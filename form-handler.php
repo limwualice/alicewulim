c37e35584345986d8018cbeb1f2571e1eb342820
Alice Wu Lim <125330688+limwualice@users.noreply.github.com>
2023-05-10 14:01:29 -0700

    Update form-handler.php
    Add honeypot field

--- form-handler.php
+++ form-handler.php
@@ -1,22 +1,30 @@
 <?php
-$name=$_POST['name'];
-$visitor_email=$_POST['email'];
-$subject=$_POST['subject'];
-$message=$_POST['message'];
+$honeypot = $_POST['honeypot']; // Retrieve the honeypot field value
 
-$email_from='alice@alicewulim.com';
-$email_subject='New Form Submission';
-$email_body="Username: $name.\n".
-            "User email: $visitor_email.\n".
-            "User message: $message .\n";
-$to='limwualice@gmail.com';
+if (!empty($honeypot)) {
+    // Handle the bot submission (e.g., log, reject, or perform other actions).
+    // You can choose to redirect or display an error message.
+    header("Location: error.html");
+    exit();
+} else {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$headers="From: $email_from \r\n";
+    $email_from = 'alice@alicewulim.com';
+    $email_subject = 'New Form Submission';
+    $email_body = "Username: $name.\n" .
+        "User email: $visitor_email.\n" .
+        "User message: $message.\n";
+    $to = 'limwualice@gmail.com';
 
-$headers .= "Reply-To: $visitor_email \r\n";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+    mail($to, $email_subject, $email_body, $headers);
 
-header("Location: contact.html");
-
+    header("Location: contact.html");
+    exit();
+}
 ?>
